a7fadec4a63ebb32c92f950c617bf73919498c4f
Anita Baran <anitabaran@MacBook-Pro-Anita.local>
2018-01-26 18:05:52 +0100

    work on TaskList

--- src/Components/TasksList.js
+++ src/Components/TasksList.js
@@ -1,14 +1,68 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import green from 'material-ui/colors/green';
+import {ListItem, ListItemSecondaryAction, ListItemText} from 'material-ui/List';
+import {FormControlLabel} from 'material-ui/Form';
+import Checkbox from 'material-ui/Checkbox';
+import IconButton from 'material-ui/IconButton';
+import DeleteIcon from 'material-ui-icons/Delete';
+
+const style = {
+    checked: {
+        color: green[500],
+        textDecoration: 'line-through',
+    },
+    unChecked: {}
+};
+
 
 class TasksList extends Component {
-    render() {
-        return (
-            <div>
-                Tasks List
-            </div>
-        );
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {
+            checked: false
+        }
     }
+
+    handleChange = name => event => {
+        this.setState({ [name]: event.target.checked });
+    };
+
+    render() {
+        const { classes } = this.props;
+        return (
+        <ListItem>
+
+            <FormControlLabel
+                control={
+                    <Checkbox
+                        checked={this.state.checkedG}
+                        onChange={this.handleChange('checkedG')}
+                        classes={{checked: classes.checked}}
+                        value="checkedG"
+                    />
+                }
+            />
+
+            <ListItemText primary={this.props.name} style={this.state.checked ? style.checked : style.unChecked}/>
+
+            <ListItemSecondaryAction>
+                <IconButton aria-label="Delete">
+                    <DeleteIcon onClick={this.props.onDelete}/>
+                </IconButton>
+            </ListItemSecondaryAction>
+
+        </ListItem>
+    );
+    }
+
 }
 
+TasksList.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
 
-export default TasksList;
+
+export default withStyles(style)(TasksList);
